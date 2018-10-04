5e683433b10fc312526fef4937da39c0cbff592e
Aluno Enap <aluno@enap.df>
2018-10-04 12:00:16 -0300

    Aula 05

--- Aula4/Aula4.R
+++ Aula4/Aula4.R
@@ -1,7 +1,7 @@
 #### Aula4
 
-### Instalando Pacotes
-# Exercícios aula 04
+### Instalando Pacotes ## macetinhos
+# Exercícios aula 04 
 lista.de.pacotes = c("tidyverse","lubridate","janitor","readxl","stringr","repmis", "readr") # escreva a lista de pacotes
 novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
                                       installed.packages()[,"Package"])]
@@ -37,16 +37,14 @@ write_rds(juizes_drogas_CL,"E:/ENAP_Especializacao/D6_Analise_de_dados/aulas_ena
 # Faça commit e push do script e do arquivo `.rds` ----
 ##########################################################################################Segunda Parte da Aula
 ## Usando Tidyverse Gather
-decisoes_gather <- decisoes %>%
+(decisoes_gather <- decisoes %>%
   filter(!is.na(id_decisao)) %>%
   select(id_decisao:data_registro) %>% 
   # 1. nome da coluna que vai guardar os nomes de colunas empilhadas
   # 2. nome da coluna que vai guardar os valores das colunas
   # 3. seleção das colunas a serem empilhadas
   gather(key="variavel", value="valor",-id_decisao)%>% 
-  arrange(id_decisao)
-
-decisoes_gather
+  arrange(id_decisao))
 
 ## Usando Tidyverse Spread
 decisoes_spread <- decisoes %>% 
@@ -66,16 +64,53 @@ juizes_drogas_novo <- decisoes %>%
   filter (!is.na(txt_decisao)) %>% 
   ##criando a variavel droga
   mutate(txt_decisao = tolower(txt_decisao),
-         droga = str_detect(txt_decisao, "droga|entorpecente|psicotr[óo]pico|maconha|haxixe|coca[íi]na"), 
+         coluna_droga = str_detect(txt_decisao, "droga|entorpecente|psicotr[óo]pico|maconha|haxixe|coca[íi]na"), 
   ## filtrando
-  droga = case_when(
-    droga == TRUE ~ "droga",
-    droga == FALSE ~ "n_droga"
+  coluna_droga = case_when(
+    coluna_droga == TRUE ~ "droga",
+    coluna_droga == FALSE ~ "n_droga"
   )) %>%
-  group_by (juiz, droga) %>%
-  summarize (qtd_droga = n())
-   
+  ## agrupando
+  group_by (juiz, coluna_droga) %>%
+  ##mesma coisa de n = n() criando uma coluna
+  summarize (qtd_droga = n())  %>%
+  ## dividindo a coluna_droga em duas colunas pelos seus valores de quantidade de drogas
+  spread (coluna_droga, qtd_droga, fill = 0) %>%
+  ## adicionando uma coluna de totale uma coluna de proporção
+  mutate(total= droga+n_droga, proporcao = droga/total)  
 
+
+
+##########################################Aula 5 ##########################################################
+## Qual a quantidade de decições mensal por juiz.
+juiz_mes <-  decisoes %>%
+  ## Atualizando uma coluna
+  mutate(data_decisao = dmy(data_decisao),
+         mes = month(data_decisao)) %>%
+  ##limpando os vazios
+  filter(!is.na(mes)) %>%
+  ## agruppando e criando uma nova coluna de contagens
+  group_by(juiz, mes) %>%
+  summarise (qtde_julgamentos = n()) %>%
+  ## Criando colunas pela informação mês
+  spread (mes,qtde_julgamentos, fill= 0)
+  
+## separando uma coluna em duas
+decisoes_sep <-  decisoes %>%
+  select(n_processo,classe_assunto) %>%
+  separate(classe_assunto, c('classe','assunto'),sep = '/', extra = 'merge', fill = 'right') %>%
+  count(assunto, sort = TRUE)
+
+#####################################Trabalhando com arquivos de dados aninhados
+###  Carregando o arquivo
+setwd("D:/ENAP_Especializacao/D6_Analise_de_dados/aulas_enap/dados/")
+processos <- readRDS("processos_nested.rds")
+
+d_partes <- processos %>%
+  select(n_processo, partes) %>%
+  unnest(partes)
+View(d_partes)
+             
 # Crie um objeto contendo informações sobre os tamanhos das bancadas dos ----
 # partidos (arquivo `bancadas.rds`), suas respectivas coligações 
 # eleitorais para 2018 (arquivo `coligacoes.xlsx`) e o 
